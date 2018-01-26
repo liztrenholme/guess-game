ef658acb629fa149f2d21b87b52af73a9e25dfbe
Liz T <32151271+liztrenholme@users.noreply.github.com>
2018-01-26 11:40:52 -0500

    Update guessgame.js

--- guessgame.js
+++ guessgame.js
@@ -1,17 +1,20 @@
 var num = Math.floor(((Math.random()* 10) + 1));
 var guess = prompt("Guess a number between 1 and 10!");
 var guessNum = parseInt(guess);
-var tries = [guessNum];
+var tries = []; //creating an array to feed number of guesses into
 
 function guessTheNum() {
-	if (guess > num) {
+	if (guess > num) { //adding high guesses into tries array
 		for (var i = 0; i < guess.length; i++) {
 			(tries.push(guess) + (i + 1));
-		}
+		} 
 		alert("Too high!");
 		guessAgain();
 	}
-	else if (guess < num) {
+	else if (guess < num) { //adding low guesses into tries array
+		for (var i = 0; i < guess.length; i++) {
+			(tries.push(guess) + (i + 1));
+		} 
 		alert("Too low!");
 		guessAgain();
 	}
@@ -24,9 +27,12 @@ function guessAgain() {
 	}
 }
 
-guessTheNum();
+guessTheNum(); //calling the function to start the game, which then calls guessAgain function within itself for as many times is as needed
 
+if (guess === num) {
+	alert("Hooray!  You got it!")
+}
 
-alert("It was " + num + "!");
+alert("It was " + num + "!" + " You guessed " + tries.length + " time(s), and " + tries + " were your guesses!"); 
+//tells correct number, tells user how many guess they made, repeats back user's guesses
 
-alert("You guessed " + tries.length + " time(s)!");
