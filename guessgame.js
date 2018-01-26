8b53d39e98ab8eda325b596283731aebe8d548b2
Liz T <32151271+liztrenholme@users.noreply.github.com>
2018-01-25 23:50:24 -0500

    Update guessgame.js

--- guessgame.js
+++ guessgame.js
@@ -1,9 +1,13 @@
 var num = Math.floor(((Math.random()* 10) + 1));
 var guess = prompt("Guess a number between 1 and 10!");
 var guessNum = parseInt(guess);
+var tries = [guessNum];
 
 function guessTheNum() {
 	if (guess > num) {
+		for (var i = 0; i < guess.length; i++) {
+			(tries.push(guess) + (i + 1));
+		}
 		alert("Too high!");
 		guessAgain();
 	}
@@ -25,3 +29,4 @@ guessTheNum();
 
 alert("It was " + num + "!");
 
+alert("You guessed " + tries.length + " time(s)!");
